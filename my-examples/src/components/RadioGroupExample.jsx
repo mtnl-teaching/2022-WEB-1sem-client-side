6f9141f816338cc004ad2c9300fc869477edd4ef
cph-mtnl <mtnl@cphbusiness.dk>
2022-11-17 13:17:41 +0100

    now with controlled radio inputs

--- my-examples/src/components/RadioGroupExample.jsx
+++ my-examples/src/components/RadioGroupExample.jsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
+
 export default function RadioGroupExample() {
+  const [theme, setTheme] = useState("");
+
+  const switchTheme = (event) => {
+    setTheme(event.target.value);
+  };
+
   return (
     <div className="default-div">
       <form>
-        <input type={"radio"} name="theme" value={"dark"} id="dark" required />
+        <input
+          checked={theme === "dark"}
+          type={"radio"}
+          name="theme"
+          value={"dark"}
+          id="dark"
+          onChange={switchTheme}
+          required
+        />
         <label htmlFor="dark">Dark</label>
         <input
+          checked={theme === "light"}
           type={"radio"}
           name="theme"
           value={"light"}
           id="light"
+          onChange={switchTheme}
           required
         />
         <label htmlFor="light">Light</label>
