52a54d894c5514cb033ebe98c78de6d042239d10
cph-mtnl <mtnl@cphbusiness.dk>
2022-11-17 11:17:51 +0100

    React Router example

--- week-46-v2/src/index.js
+++ week-46-v2/src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import RRApp from "./RRApp";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <RRApp />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
